60df3604f545aac8178239e994f16db903c84560
Adrian Valladares <Adrian.Valladares@brightstar.com>
2022-05-10 23:07:39 -0500

    etapa 2

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require ('path');
 
 app.listen(3000, () => console.log("En orden"));
-app.use ( express.static( path.join(__dirname, './Mercadoliebre/public')));
+app.use ( express.static( path.join(__dirname, './public')));
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.resolve('./Mercadoliebre/views/index.html'));
+    res.sendFile(path.resolve('./views/index.html'));
 })
